635a2d0b6c1c6369651d0deae3e1e950132195eb
Kaustuv Mukherjee <kaustuv.mukherjee@kp.org>
2018-12-29 12:28:21 -0800

    Now generates random string fields.

--- src/generator/generate.js
+++ src/generator/generate.js
@@ -4,15 +4,20 @@
 'use strict'
 const fs = require('fs')
 const winston = require('winston')
+const randomstring = require("randomstring")
 const utility = require('../utility/utility')
 
-let size = 10
+let size = 10 // should not be more than  26
 const prefixLookUp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
                         'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 const srcPath = fs.realpathSync('src')
 
+const fields_with_random_alphabets = true
+
 class Generate {
-    static generateData(noOfFields, noOfRecords) {
+    static generateData(sizeOfEachField,
+                        noOfFields,
+                        noOfRecords) {
         // If data directory does not exists, create it
         try {
             fs.mkdirSync(`${srcPath}/data`)
@@ -29,21 +34,41 @@ class Generate {
             winston.info(`File not found !!`)
         }
 
-        for(let i = 0; i < noOfRecords; i++) {
-            let record = '{'
-            let index = (i).pad(size)
-            for(let j = 0; j < noOfFields; j++) {
-                let key = `\"key${j}\"`
-                let prefix = prefixLookUp[j%prefixLookUp.length]
-                let value = `\"${prefix}${index}\"`
-                record += `${key}: ${value}`
-                if(j !== noOfFields-1) {
-                    record += `,`
-                } else {
-                    record += '}\n'
+        if(fields_with_random_alphabets) {
+            for(let i = 0; i < noOfRecords; i++) {
+                let record = '{'
+                for(let j = 0; j < noOfFields; j++) {
+                    let value = randomstring.generate({
+                        length: sizeOfEachField,
+                        charset: 'alphabetic'
+                    })
+                    let key = `key${j}`
+                    record += `\"${key}\": \"${value}\"`
+                    if(j !== noOfFields-1) {
+                        record += `,`
+                    } else {
+                        record += '}\n'
+                    }
                 }
+                fs.appendFileSync(outFile, record)
             }
-            fs.appendFileSync(outFile, record)
+        } else {
+            for(let i = 0; i < noOfRecords; i++) {
+                let record = '{'
+                let index = (i).pad(sizeOfEachField)
+                for(let j = 0; j < noOfFields; j++) {
+                    let key = `\"key${j}\"`
+                    let prefix = prefixLookUp[j%prefixLookUp.length]
+                    let value = `\"${prefix}${index}\"`
+                    record += `${key}: ${value}`
+                    if(j !== noOfFields-1) {
+                        record += `,`
+                    } else {
+                        record += '}\n'
+                    }
+                }
+                fs.appendFileSync(outFile, record)
+            }
         }
     }
 }
--- src/generator/generator.js
+++ src/generator/generator.js
@@ -5,4 +5,10 @@
 const winston = require('winston')
 const generate = require('./generate')
 
-generate.generateData(20, 100000)
+const size_of_each_field = 30
+const no_of_fields_per_record = 2
+const no_of_records = 100
+
+generate.generateData(size_of_each_field <= 26 ? size_of_each_field : 26,
+    no_of_fields_per_record,
+    no_of_records)
